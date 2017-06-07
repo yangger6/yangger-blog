e4f04307eccebc082fe382007ce95865990973fe
yangger6 <18902833810@163.com>
2017-06-07 21:22:47 +0800

    "铺设博客头部"

---
+++ src/components/list/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ROBOT on 2017/6/6.
+ */
+import list from './list.vue'
+export default function install (Vue) {
+  Vue.component('list', list)
+}
---
+++ src/components/list/list.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul :class="size">
+    <slot></slot>
+  </ul>
+</template>
+<style scoped rel="stylesheet/scss" lang="scss">
+  @import "list";
+</style>
+<script>
+  export default{
+    props: {
+      size: String  // 导航栏的大小 dense medium large
+    },
+    data () {
+      return {
+      }
+    }
+  }
+</script>
