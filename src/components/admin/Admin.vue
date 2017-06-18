2396531ec9121aad0428bfbb9c90064b6545415a
yangger6 <18902833810@163.com>
2017-06-18 23:43:07 +0800

    "博客管理系统样式铺设，暂时偷ghost的样式T  T"

--- src/components/admin/Admin.vue
+++ src/components/admin/Admin.vue
@@ -1,17 +1,53 @@
 <template>
-  <div v-if="isLogin">
-
-  </div>
-  <div v-else>
-    <router-view></router-view>
+  <div class="Admin">
+    <div v-if="isLogin" class="admin">
+      <nav class="admin-nav">
+        <header>
+          <div class="admin-head">
+            <img src="/static/head.png" alt="">
+          </div>
+          <div class="admin-details">
+            <div>Blogs</div>
+            <div>Yangger</div>
+          </div>
+        </header>
+        <main>
+          <ul v-for="(item, index) in items">
+            <li><a @click="changeItem(index)" :class="{selected: index === selectIndex}"><icon :name="item.i"></icon>{{item.v}}</a></li>
+          </ul>
+        </main>
+        <footer>
+          <icon name="arrow-left"></icon>
+          <p><router-link to="/">VIEW BLOGS</router-link></p>
+        </footer>
+      </nav>
+      <main class="editor">
+        <editor></editor>
+      </main>
+    </div>
+    <div v-else>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 <style scoped rel="stylesheet/scss" lang="scss">
+  @import "admin";
 </style>
 <script>
+  import editor from './editor/editor.vue'
   export default{
     data () {
       return {
+        selectIndex: -1,
+        items: [
+          {i: 'pencil', v: 'New Post'},
+          {i: 'file-text', v: 'Context'}
+        ]
+      }
+    },
+    methods: {
+      changeItem (index) {
+        this.selectIndex = index
       }
     },
     computed: {
@@ -23,6 +59,9 @@
       if (!this.isLogin) {
         this.$router.push({name: 'Login'})
       }
+    },
+    components: {
+      editor
     }
   }
 </script>
---
+++ src/components/admin/editor/editor.vue
@@ -0,0 +1,51 @@
+<template>
+  <section class="new-Post">
+    <header>
+      <h2>
+        <button></button>
+        <input type="text" placeholder="Your Post Name">
+      </h2>
+      <section class="btn">
+        <button>{{controlText}}</button>
+        <button @click="controlShow = !controlShow"><icon name="sort-down"></icon></button>
+        <div class="dropdown">
+          <ul v-show="controlShow">
+            <li v-for="(item, index) in controlItems"><a @click="changeControl(index)">{{item}}</a></li>
+            <li class="line"></li>
+            <li><a>Delect Post</a></li>
+          </ul>
+        </div>
+      </section>
+    </header>
+    <section>
+      <textarea name="" id="" cols="30" rows="10"></textarea>
+    </section>
+    <section></section>
+  </section>
+</template>
+<style scoped rel="stylesheet/scss" lang="scss">
+  @import "editor";
+</style>
+<script>
+  export default{
+    data () {
+      return {
+        msg: 'Hello',
+        controlShow: false,
+        controlIndex: 0,
+        controlItems: ['Publish Now', 'Save Draft']
+      }
+    },
+    computed: {
+      controlText () {
+        return this.controlItems[this.controlIndex]
+      }
+    },
+    methods: {
+      changeControl (index) {
+        this.controlIndex = index
+        this.controlShow = false
+      }
+    }
+  }
+</script>
