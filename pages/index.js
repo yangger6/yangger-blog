f020d963a2eb3ce09d27a6fb6e73107918c32fb9
yangger <i@yangger.cn>
2021-07-27 14:13:34 +0800

    fix: 修复文件名错误

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import Layout from '../components/Layout'
+import Seo from '../components/Seo'
 import { fetchAPI } from '../lib/api'
 import Article from '../components/Article'
 import Pagination from '../components/Pagination'
