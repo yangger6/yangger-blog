3a67c33a6b1d977e9a0d30d56eca337e9941f906
yangger <i@yangger.cn>
2021-07-27 14:30:23 +0800

    chore: 改成静态部署

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,8 @@ import Pagination from '../components/Pagination'
 
 const Home = ({ articles, categories, homepage }) => {
   return (
-    <Layout categories={categories}>
-      <Seo seo={homepage.seo} />
+    <Layout>
+      {/*<Seo seo={homepage.seo} />*/}
       <Article type='banner' />
       <Article />
       <Article />
@@ -20,18 +20,18 @@ const Home = ({ articles, categories, homepage }) => {
     </Layout>
   )
 }
-export async function getStaticProps() {
-  // Run API calls in parallel
-  // const [articles, categories, homepage] = await Promise.all([
-  //   fetchAPI('/articles?status=published'),
-  //   fetchAPI('/categories'),
-  //   fetchAPI('/homepage'),
-  // ])
-
-  return {
-    props: { articles: {}, categories: {}, homepage: {} },
-    revalidate: 1,
-  }
-}
+// export async function getStaticProps() {
+//   // Run API calls in parallel
+//   // const [articles, categories, homepage] = await Promise.all([
+//   //   fetchAPI('/articles?status=published'),
+//   //   fetchAPI('/categories'),
+//   //   fetchAPI('/homepage'),
+//   // ])
+//
+//   return {
+//     props: { articles: {}, categories: {}, homepage: {} },
+//     revalidate: 1,
+//   }
+// }
 
 export default Home
