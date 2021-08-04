1510e89f9269b6c96408aa7674262346b405efe9
yangger <i@yangger.cn>
2021-08-04 15:10:46 +0800

    build: 使用静态文件部署

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ const isProd = process.env.NODE_ENV === 'production'
 module.exports = {
   reactStrictMode: true,
   assetPrefix: isProd ? 'https://cdn.yangger.cn' : '',
+  images: {
+    loader: 'imgix',
+    path: '',
+  },
 }
